1efcc2ba50cc75ab7b1fb4f9914a8fd03bd7c452
ARGAUD <romain943602@gmail.com>
2023-11-19 13:25:58 +0100

    Test fonctionnel

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.spoonacular.DefaultApi;
 import com.spoonacular.RecipesApi;
 import com.spoonacular.client.ApiClient;
@@ -11,43 +15,52 @@ import com.spoonacular.client.model.GetIngredientInformation200Response;
 import com.spoonacular.IngredientsApi;
 import com.spoonacular.client.model.GetRandomRecipes200Response;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Hello world!");
-        ApiClient apiInstance = getDefaultApi();
-
-
-        //apiInstance.buildCall("https://api.spoonacular.com","/food/ingredients/9251/information",)
-        RecipesApi RecipieapiInstance = new RecipesApi(apiInstance);
-        Boolean limitLicense = true; // Boolean | Whether the recipes should have an open license that allows display with proper attribution.
-        String tags = "pineapple"; // String | The tags (can be diets, meal types, cuisines, or intolerances) that the recipe must have.
-        Integer number = 1; // Integer | The maximum number of items to return (between 1 and 100). Defaults to 10.
-        try {
-            GetRandomRecipes200Response result = RecipieapiInstance.getRandomRecipes(limitLicense, tags, number);
-            System.out.println(result);
-        } catch (ApiException e) {
-            System.err.println("Exception when calling DefaultApi#analyzeRecipe");
-            System.err.println("Status code: " + e.getCode());
-            System.err.println("Reason: " + e.getResponseBody());
-            System.err.println("Response headers: " + e.getResponseHeaders());
-            e.printStackTrace();
+        HttpClient client = getDefaultApi();
+        String Baseurl = "https://api.spoonacular.com/";
+        List<String> l_ingredients = new ArrayList<String>();
+        l_ingredients.add("beans");
+        StringBuilder ingredients = new StringBuilder();
+        for(int i=0;i<l_ingredients.size();i++){
+            ingredients.append(l_ingredients.get(i));
+            if(i!=l_ingredients.size()-1) ingredients.append(",");
         }
+
+
+        // create a request
+        var request = HttpRequest.newBuilder(
+                        URI.create(Baseurl+"recipes/complexSearch?includeIngredients="+ingredients+"&number=2&sort=max-used-ingredients&addRecipeInformation=true&apiKey=338284d8348d4e2bb344a1747cb00005"))
+                .header("accept", "application/json")
+                .build();
+
+        // use the client to send the request
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(response.body());
+        //result =
+        // the response:
+        System.out.println(response);
+        System.out.println(jsonNode.get("results"));
     }
 
 
-    private static ApiClient getDefaultApi() {
-        ApiClient defaultClient = Configuration.getDefaultApiClient();
-        defaultClient.setBasePath("https://api.spoonacular.com");
-
-        // Configure API key authorization: apiKeyScheme
-        ApiKeyAuth apiKeyScheme = (ApiKeyAuth) defaultClient.getAuthentication("apiKeyScheme");
-        apiKeyScheme.setApiKey("338284d8348d4e2bb344a1747cb00005");
-        // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
-        //apiKeyScheme.setApiKeyPrefix("Token");
-
-        return defaultClient;
+    private static HttpClient getDefaultApi() {
+        // create a client
+        var client = HttpClient.newHttpClient();
+        return client;
     }
 
 
